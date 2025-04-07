33df5da25a5227eb2f27ff76643004fc84ac3ff5
jungsun0805 <sensepw7@gmail.com>
2025-04-07 16:04:47 +0900

    대용량 파일 업로드

--- foodychat_frontend/src/components/App.js
+++ foodychat_frontend/src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './MainPage';
 import Login from './Login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>FoodyChat 로그인</h1>
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
