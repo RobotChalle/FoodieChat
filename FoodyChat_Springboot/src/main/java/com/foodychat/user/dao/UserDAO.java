8877961ca572b57c803772a8cc31bede0f209fe1
RobotChalle <ubermensch01249600.com>
2025-04-10 17:25:43 +0900

    awd

--- FoodyChat_Springboot/src/main/java/com/foodychat/user/dao/UserDAO.java
+++ FoodyChat_Springboot/src/main/java/com/foodychat/user/dao/UserDAO.java
@@ -1,5 +1,9 @@
 package com.foodychat.user.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.foodychat.user.vo.UserDetailsVO;
 import com.foodychat.user.vo.UserVO;
 
@@ -11,4 +15,8 @@ public interface UserDAO {
     UserVO findByEmail(String email);             // 이메일로 사용자 조회
     void insertGoogleUser(UserVO user);           // 구글 로그인 시 새 유저 등록
     void insertUserDetails(UserDetailsVO details); //유저 세부정보 입력.
+    List<UserVO> getUserList(@Param("limit") int limit, @Param("offset") int offset); //유저 전체 목록
+    int getTotalUserCount();//유저 수 카운트
+	void deleteUser(Long userId);
+	void updateMembershipLevel(@Param("userId") Long userId, @Param("level") String level);
 }
--- foodychat_frontend/src/components/App.js
+++ foodychat_frontend/src/components/App.js
@@ -1,5 +1,3 @@
-// src/components/App.js
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
@@ -12,14 +10,14 @@ import NavBar from './NavBar';
 
 function App() {
   return (
-    <GoogleOAuthProvider clientId="758964500028-3l2f9avbost20tq4ri7nr4nfed4fd2l3.apps.googleusercontent.com"> {/* 여기에 본인 clientId 입력 */}
+    <GoogleOAuthProvider clientId="758964500028-3l2f9avbost20tq4ri7nr4nfed4fd2l3.apps.googleusercontent.com">
       <Router>
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignupForm />} />
           <Route path="/user-details" element={<UserDetailsForm />} />
-          <Route path="/adminpage" element={<AdminPage />} />
+          <Route path="/admin/users" element={<AdminPage />} />
           <Route path="/navbar" element={<NavBar />} />
         </Routes>
       </Router>
