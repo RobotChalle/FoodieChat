ea43a39ef21f13026e4b5f881f94304fe6f5a93d
jungsun0805 <sensepw7@gmail.com>
2025-04-18 10:33:46 +0900

    111

--- FoodyChat_Springboot/src/main/java/com/foodychat/config/GlobalCorsConfig.java
+++ FoodyChat_Springboot/src/main/java/com/foodychat/config/GlobalCorsConfig.java
@@ -2,6 +2,7 @@ package com.foodychat.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -9,15 +10,25 @@ public class GlobalCorsConfig {
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 	    return new WebMvcConfigurer() {
-			/*
-			 * @Override public void addCorsMappings(CorsRegistry registry) {
-			 * registry.addMapping("/**") .allowedOrigins("http://localhost:8080")
-			 * .allowedMethods("GET", "POST", "PUT", "DELETE") .allowCredentials(true); }
-			 */
-			// 개발시 위에 주석하고 밑에 소스 사용
-			@Override public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) { 
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") .allowCredentials(true); // 세션 공유용 
+			
+			 @Override public void addCorsMappings(CorsRegistry registry) {
+				 registry
+				 	.addMapping("/**")
+				 	//.allowedOrigins("http://192.168.0.29:8080")
+					.allowedOrigins("http://localhost:8080")
+				 	.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				 	.allowCredentials(true);
 			}
+			 
+			// 개발시 위에 주석하고 밑에 소스 사용
+			/*
+			 * @Override public void
+			 * addCorsMappings(org.springframework.web.servlet.config.annotation.
+			 * CorsRegistry registry) {
+			 * registry.addMapping("/**").allowedOrigins("http://localhost:3000").
+			 * allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+			 * .allowCredentials(true); // 세션 공유용 }
+			 */
 	    };
 	}
 }
--- foodychat_python/foodychat_python/main.py
+++ foodychat_python/foodychat_python/main.py
@@ -32,7 +32,8 @@ app.include_router(chat_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    #allow_origins=["http://192.168.0.29:8080"],
+    allow_origins=["http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
