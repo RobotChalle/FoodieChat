bc7bd2f96afce1e7f28178dcb6dfe7a2563a0d40
jungsun0805 <sensepw7@gmail.com>
2025-04-24 17:07:18 +0900

    20250424_수정본

--- foodychat_frontend/src/index.js
+++ foodychat_frontend/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ✅ 꼭 필요합니다
 import App from './components/App';
 // import './index.css'; // (선택) 스타일 파일
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/foodychat">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
